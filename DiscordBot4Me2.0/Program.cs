83113cbdfdce71d12c976daae31b2143b06d8376
SpaceBeeGaming <space.bee.gaming@gmail.com>
2019-01-13 11:17:50 +0200

    Added solution, project and Unit test project.

---
+++ DiscordBot4Me2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBot4Me2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
