61ace443e10d4b3e58bf00afd1d915b2cd98a65f
SpaceBeeGaming <space.bee.gaming@gmail.com>
2019-01-14 18:25:09 +0200

    Various fixes and improvements to storage unit testing - TestFileSystemStorage unit tests use mock filesystem. - TestFileSystemStorage now use assert. - Fixed bugs in JsonStorage.

--- DiscordBotForMe/Storage/Implementations/JsonStorage.cs
+++ DiscordBotForMe/Storage/Implementations/JsonStorage.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO.Abstractions;
 
 using Newtonsoft.Json;
 
@@ -6,29 +7,39 @@ namespace DiscordBotForMeCore.Storage.Implementations
 {
     public class JsonStorage : IDataStorage
     {
+        private readonly IFileSystem fileSystem;
+
+        public JsonStorage() => fileSystem = new FileSystem();
+
+        public JsonStorage(IFileSystem fileSystem) => this.fileSystem = fileSystem;
+
         public T RestoreObject<T>(string key)
         {
             string file = ValidPath(key);
-            string json = File.ReadAllText(file);
+            if (!fileSystem.File.Exists(file)) throw new ArgumentException("Key must point to a File, without extention.");
+            string json = fileSystem.File.ReadAllText(file);
             return JsonConvert.DeserializeObject<T>(json);
         }
 
         public void Store(object obj, string key)
         {
+            if (obj == null) throw new ArgumentNullException(nameof(obj));
             string file = ValidPath(key);
-            string directory = Path.GetDirectoryName(key);
+            string directory = fileSystem.Path.GetDirectoryName(key);
+            bool debugBool = !string.IsNullOrWhiteSpace(directory);
             if (!string.IsNullOrWhiteSpace(directory))
             {
-                Directory.CreateDirectory(directory);
-
+                fileSystem.Directory.CreateDirectory(directory);
             }
             string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
-            File.WriteAllText(file, json);
+            fileSystem.File.WriteAllText(file, json);
         }
 
         private string ValidPath(string path)
         {
-            if (string.IsNullOrWhiteSpace(path)) throw new System.ArgumentException("Path is null or whitespace!", nameof(path));
+            if (string.IsNullOrWhiteSpace(path)) throw new ArgumentException("Path is null or whitespace!", nameof(path));
+            if (path.Contains(".json")) throw new ArgumentException("Key must point to a File, without extention.");
+
             return $"{path}.json";
         }
     }
---
+++ UnitTests.xUnit/TestFileSystemStorage.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Abstractions.TestingHelpers;
+
+using DiscordBotForMeCore.Storage;
+using DiscordBotForMeCore.Storage.Implementations;
+
+using Newtonsoft.Json;
+
+using Xunit;
+
+namespace UnitTests.xUnit
+{
+    public class TestFileSystemStorage
+    {
+        [Theory]
+        [InlineData("TestString", "Config/Test")]
+        [InlineData(1, "Config/Test")]
+        [InlineData("TestString", "Test")]
+        [InlineData("", "Config/Test")]
+        [InlineData(new int[] { 1, 4, 7, 0 }, "Config/Test")]
+        [InlineData(new string[] { "Hello", "Test", "Blah" }, "Config/Test")]
+        public void Store_ShouldStore(object obj, string key)
+        {
+            MockFileSystem fileSystem = new MockFileSystem();
+
+            IDataStorage dataStorage = new JsonStorage(fileSystem);
+            dataStorage.Store(obj, key);
+
+            Assert.True(fileSystem.File.Exists(key + ".json"));
+        }
+
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("Config/Test.json")]
+        [InlineData("Test.json")]
+        public void Store_InvalidKey_ShouldThrow(string key)
+        {
+            const string obj = "TestObject";
+
+            MockFileSystem fileSystem = new MockFileSystem();
+
+            IDataStorage dataStorage = new JsonStorage(fileSystem);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                dataStorage.Store(obj, key);
+            });
+        }
+
+        [Fact]
+        public void Store_nullObject_ShouldThrow()
+        {
+            const string key = "ABC";
+
+            MockFileSystem fileSystem = new MockFileSystem();
+
+            IDataStorage dataStorage = new JsonStorage(fileSystem);
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                dataStorage.Store(null, key);
+            });
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Restore_InvalidKey_ShouldThrow(string key)
+        {
+            MockFileSystem fileSystem = new MockFileSystem();
+
+            IDataStorage dataStorage = new JsonStorage(fileSystem);
+            Assert.Throws<ArgumentException>(() =>
+            {
+                dataStorage.RestoreObject<object>(key);
+            });
+        }
+
+        [Fact]
+        public void Restore_WrongKey_ShouldThrow()
+        {
+            const string invalidKey = "bdldkf";
+
+            MockFileSystem fileSystem = new MockFileSystem();
+
+            IDataStorage dataStorage = new JsonStorage(fileSystem);
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                dataStorage.RestoreObject<string>(invalidKey);
+            });
+        }
+
+        [Theory]
+        [InlineData("TestString", "Config/Test")]
+        [InlineData("TestString", "Test")]
+        [InlineData("", "Config/Test")]
+        [InlineData(new int[] { 1, 4, 7, 0 }, "Config/Test")]
+        [InlineData(new string[] { "Hello", "Test", "Blah" }, "Config/Test")]
+        public void Restore_ShouldRestore(object toRestore, string key)
+        {
+
+            MockFileSystem fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+            {
+                {@"c:/"+key+".json", new MockFileData(GetComparisonElements(toRestore).jsonString)}
+            });
+
+            IDataStorage dataStorage = new JsonStorage(fileSystem);
+            object restored = dataStorage.RestoreObject<object>(key);
+
+            Assert.True(GetComparisonElements(toRestore).deserializedJsonString.ToString() == restored.ToString());
+        }
+
+        private (string jsonString, object deserializedJsonString) GetComparisonElements(object obj)
+        {
+            string jsonString = JsonConvert.SerializeObject(obj);
+            object output = JsonConvert.DeserializeObject<object>(jsonString);
+            return (jsonString, output);
+        }
+    }
+}
